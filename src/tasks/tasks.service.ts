98470788b7077372fcb42838ea3b906335712012
chediak <shammcity00@gmail.com>
2020-11-11 14:06:31 -0300

    pipes

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -32,7 +32,12 @@ export class TasksService {
     }
 
     getTaskById(id: string): Task {
-        return this.tasks.find(task => task.id === id);
+        const found = this.tasks.find(task => task.id === id);
+
+        if(!found) {
+            throw new NotFoundException(`Task with ID "${id}" not found`);
+        }
+        return found;
     }
 
     createTask(createTaskDto: CreateTaskDto): Task {
@@ -50,7 +55,8 @@ export class TasksService {
     }
 
     deleteTask(id: string): void {
-        this.tasks = this.tasks.filter(task => task.id !== id);
+        const found = this.getTaskById(id);
+        this.tasks = this.tasks.filter(task => task.id !== found.id);
     }
 
     updateTaskStatus(id: string, status: TaskStatus): Task {
